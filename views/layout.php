f95bcd098e542064c08c082ba79da80d4e8168ba
MassiPozzuto <massipozzuto2016@gmail.com>
2023-08-11 15:24:22 -0300

    Agrego layout y linkeo con bootstrap y jquery

---
+++ views/layout.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es" data-theme="light">
+
+<head>
+  <title><?php echo $page; ?> • Fooder</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Viex Inc.">
+
+  <link rel="icon" sizes="192x192" href="">
+
+  
+  <!-- FONTS -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+  <!-- BOOTSTRAP -->
+  <script src="../js/bootstrap.bundle.js"></script>
+  <link href="../css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+  <!-- JQUERY -->
+  <script src="../js/jquery.min.js"></script>
+  <!-- CSS -->
+  
+
+</head>
+
+<body>
+
+  <?php
+  // Traigo el header
+  require_once('../includes/header.php');
+  ?>
+
+  <!-- Empieza el contenido especifico -->
+  <div class="container">
+    <?php require_once($section . ".php") ?>
+  </div>
+  <!-- Termina el contenido especifico -->
+
+  <script src="../js/main.js" type="text/javascript"></script>
+</body>
+
+</html>
