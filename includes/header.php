6ee64698867c03221a559cfc1f1b5daa6bdc93de
MassiPozzuto <massipozzuto2016@gmail.com>
2023-08-18 16:41:34 -0300

    Agrego header completo

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,48 @@
-<header class="main__header">
-    <div class="header__logo">
-        LOGO
-    </div>
+<header>
     
-    <ul class="header__menu">
-        <li><a href="">GESTIONAR</a></li>
-        <li><a href="">RESERVAS</a></li>
-        <li><a href="">INFORMACION</a></li>
-        <li><a href="">AYUDA</a></li>
+    <button class="btn list_menu"><i class="bi bi-list" id="icon-list"></i></button>
+    
+    <div class="container__logo">
+        <img src="../img/racedo logo.png" alt="Logo empresa" width="85px">
+    </div>
+
+    <ul class="container__list-buttons">
+        <li class="container__btn-home">
+            <a href="">RESERVAR</a>
+        </li>
+        <li class="container_dropdown" id="container_dropdown-manage">
+            <a href="" class="">GESTIONAR</a>
+            <ul class="container__dropdown--header dropdown dropdown-manage">
+                <li><a href="">Información de mi reserva</a></li>
+                <li><a href="">Cambio de asiento</a></li>
+                <li><a href="">Cambio de clase</a></li>
+                <li><a href="">Cancelar reserva</a></li>
+            </ul>
+
+        </li>
+        <li class="container_dropdown" id="container_dropdown-info">
+            <a href="" class="">INFORMACION</a>
+            <ul class="container__dropdown--header dropdown dropdown-info">
+                <li><a href="">Quienes somos</a></li>
+                <li><a href="">La flota de aviones</a></li>
+                <li><a href="">Gastronomia de los vuelos</a></li>
+            </ul>
+
+        </li>
+        <li>
+            <a href="">AYUDA</a>
+        </li>
+
     </ul>
 
-    <div class="header__profile">
-        <a href="#" class=""><i class="bi bi-person-circle"></i></a>
+    <div class="container__profile container_dropdown" id="container_dropdown-profile2">
+        <a href="../controllers/profile.php" class="btn-profile">
+            <i class="bi bi-person-circle"></i>
+        </a>
+        <ul class="container__dropdown--header dropdown dropdown-profile2">
+            <li><a href="">Mi información</a></li>
+            <li><a href="">Editar información</a></li>
+            <li><a href="">Cerrar sesión</a></li>
+        </ul>
     </div>
 </header>
