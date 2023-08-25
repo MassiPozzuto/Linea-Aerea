b7e2b72bd111d19404294d6f12f59e8a01a1a2b5
Tsifok <hermanmatias03@gmail.com>
2023-08-25 14:45:21 -0300

    vos

--- views/register.php
+++ views/register.php
@@ -17,8 +17,7 @@
                     <div class="card card-registration my-4">
                         <div class="row g-0">
                             <div class="col-xl-6 d-none d-xl-block">
-                                <img src="../img/gettyimages-528363573-091201.jpg" alt="Imagen de registro"
-                                    class="img-fluid" style="border-radius: 1rem 0 0 1rem;">
+                                <img src="../img/gettyimages-528363573-091201.jpg" alt="Imagen de registro" class="img-fluid" style="border-radius: 1rem 0 0 1rem;">
                             </div>
                             <div class="col-xl-6">
                                 <div class="card-body p-md-5 text-black">
@@ -28,13 +27,11 @@
                                         <div class="row">
                                             <div class="col-md-6">
                                                 <label class="form-label" for="form3Example1m">Nombre:</label>
-                                                <input type="text" id="form3Example1m"
-                                                    class="form-control form-control-lg">
+                                                <input type="text" id="form3Example1m" class="form-control form-control-lg">
                                             </div>
                                             <div class="col-md-6">
                                                 <label class="form-label" for="form3Example1n">Apellido:</label>
-                                                <input type="text" id="form3Example1n"
-                                                    class="form-control form-control-lg">
+                                                <input type="text" id="form3Example1n" class="form-control form-control-lg">
                                             </div>
                                         </div>
                                     </div>
@@ -47,8 +44,7 @@
                                     <div class="mb-4">
                                         <label class="form-label" for="form3Example9">Contraseña: </label>
                                         <div class="input-group">
-                                            <input type="password" id="form3Example9"
-                                                class="form-control form-control-lg">
+                                            <input type="password" id="form3Example9" class="form-control form-control-lg">
                                             <button id="togglePassword" class="btn btn-outline-secondary" type="button">
                                                 <i class="fa fa-eye" aria-hidden="true"></i>
                                             </button>
@@ -57,8 +53,7 @@
                                             contener al menos un número.</p>
                                         <p class="warning" style="color: red; display: none;">La contraseña debe
                                             contener al menos una letra mayúscula.</p>
-                                        <p id="lengthCounter" style="display: none;">Longitud de contraseña: <span
-                                                id="lengthValue">0</span></p>
+                                        <p id="lengthCounter" style="display: none;">Longitud de contraseña: <span id="lengthValue">0</span></p>
                                     </div>
 
                                     <script>
@@ -84,8 +79,7 @@
                                             lengthValue.style.color = password.length > 8 ? 'green' : 'black';
                                         });
                                     </script>
-                                    <link rel="stylesheet"
-                                        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+                                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
                                     <div class="mb-4">
                                         <label class="form-label" for="form3Example9">Teléfono:</label>
