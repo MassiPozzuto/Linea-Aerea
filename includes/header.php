72cf0a92ef1b0f6c0643db21d82510436ce10354
Ezequiel12D <ezequiel.sosa.et26@gmail.com>
2023-08-25 15:57:37 -0300

    quienes somos

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,7 @@
         <li class="container_dropdown" id="container_dropdown-info">
             <a href="" class="">INFORMACION</a>
             <ul class="container__dropdown--header dropdown dropdown-info">
-                <li><a href="">Quienes somos</a></li>
+                <li><a href="views/quienessomos.php">Quienes somos</a></li>
                 <li><a href="">La flota de aviones</a></li>
                 <li><a href="">Gastronomia de los vuelos</a></li>
             </ul>
---
+++ views/quienessomos.php
@@ -0,0 +1,92 @@
+<link href="../css/style.css" rel="stylesheet">
+<!-- BOOTSTRAP -->
+<script src="../js/bootstrap.bundle.js"></script>
+<link href="../css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<!-- JQUERY -->
+<script src="../js/jquery.min.js"></script>
+<!-- CSS -->
+<link href="../css/main.css" rel="stylesheet">
+<link href="../css/<?php echo $section ?>.css" rel="stylesheet">
+
+<?php
+// Traigo el header
+require_once('../includes/header.php');
+?>
+
+<section class="quienes-somos">
+    <h2>Quiénes Somos</h2>
+    <p>Me destapé otro Moët
+        Mi perra viste de Chanel
+        Estamo' fresco', estamo' bien
+        Muchos billete' de 100
+        Ahora que estamo' en subida
+        Tenemo' otra perspectiva
+        Voy con el Duko y todo' miran
+        Las gatas nos aman, los gatos nos tiran
+        Me destapé otro Moët
+        Fumo y grabo en la suite del hotel (uh)
+        Me quieren dar y no me pueden ver
+        Ya no es top five, ahora es top tres
+        Ahora que estamos en subida
+        Todo' nos quieren en su vida
+        Voy con C.R.O y nos miran
+        Duko, you know
+        Fumando queso, cheesy
+        Ellos ratone', Mickey
+        Están infectado' de hepatitis
+        Escucho sus tema' y me agarra otitis
+        Mi vida, una curva, drifting
+        Estoy como nuevo, lifting
+        Antes de salir, el fitting
+        Te cruzo en la calle en fifteen
+        Muero en un escenario
+        En un harakiri vestido de AMIRI
+        Su futuro es negro, el color de mi iris
+        Si estoy con C.R.O en el estudio
+        Los tema' se montan más grande' que un remix
+        Andá y advertile', avisale', decile'
+        Que estoy en la mía, juntando los miles
+        Los giles que tiren, que opinen, que miren
+        Que, por estos lado', los pibes la viven
+        Mis olas de flow te dejan 20 metros
+        Debajo del mar hablando con delfines
+        Hago que se retiren y vuelvan tantas veces
+        A la music que les tienen que dar un Guinness
+        Los tengo enganchaos, drug dealer
+        Esta peli no la ve' en el cine
+        Lo que hacen en un año, lo hago en un finde
+        Mi hermano trajo misiles
+        Estábamos abajo, ahora estamos en la cima
+        Ropa de diseñador hecha a mi medida
+        Estoy con mi hermano jugando en las grandes ligas
+        Me espera el dinero si aterrizo en Argentina
+        La money me tiene despierto, gano dinero y lo invierto
+        Negociador experto, si no es de dinero, no entiendo
+        Follow, okey (yeah), brilla mi chain (yeah)
+        Otro champagne (yeah), ¿dónde está mi Moët?
+        Voy a beber otro Moët
+        Voy a compra' un Mercedes-Benz
+        Voy a gastar otro de 100
+        Voy a fumar en Calvin Klein
+        Voy a pasarme el puto juego
+        No me molesta estar primero
+        Voy a comprar un bolso nuevo
+        Solo para guardar dinero
+        Me destapé otro Moët
+        Mi perra viste de Chanel
+        Estamo' fresco', estamo' bien
+        Muchos billete' de 100
+        Ahora que estamo' en subida
+        Tenemo' otra perspectiva
+        Voy con el Duko y todo' miran
+        Las gatas nos aman, los gatos nos tiran
+        Me destapé otro Moët
+        Fumo y grabo en la suite del hotel (uh)
+        Me quieren dar y no me pueden ver
+        Ya no es top five, ahora es top tres
+        Ahora que estamos en subida
+        Todo' nos quieren en su vida
+        Voy con C.R.O y nos miran
+        Fumando queso, cheesy</p>
+</section>
