15ba11e38dd4b8f0ff568a3fff2c997d164c0989
Ezequiel12D <Ezequiel.sosa.et26@gmail.com>
2023-10-20 16:50:09 -0300

    Gracias Massimo por tanto

--- views/quienessomos.php
+++ views/quienessomos.php
@@ -1,76 +1,40 @@
 <section class="quienes-somos">
-    <h2>Quiénes Somos</h2>
-    <p>Me destapé otro Moët
-        Mi perra viste de Chanel
-        Estamo' fresco', estamo' bien
-        Muchos billete' de 100
-        Ahora que estamo' en subida
-        Tenemo' otra perspectiva
-        Voy con el Duko y todo' miran
-        Las gatas nos aman, los gatos nos tiran
-        Me destapé otro Moët
-        Fumo y grabo en la suite del hotel (uh)
-        Me quieren dar y no me pueden ver
-        Ya no es top five, ahora es top tres
-        Ahora que estamos en subida
-        Todo' nos quieren en su vida
-        Voy con C.R.O y nos miran
-        Duko, you know
-        Fumando queso, cheesy
-        Ellos ratone', Mickey
-        Están infectado' de hepatitis
-        Escucho sus tema' y me agarra otitis
-        Mi vida, una curva, drifting
-        Estoy como nuevo, lifting
-        Antes de salir, el fitting
-        Te cruzo en la calle en fifteen
-        Muero en un escenario
-        En un harakiri vestido de AMIRI
-        Su futuro es negro, el color de mi iris
-        Si estoy con C.R.O en el estudio
-        Los tema' se montan más grande' que un remix
-        Andá y advertile', avisale', decile'
-        Que estoy en la mía, juntando los miles
-        Los giles que tiren, que opinen, que miren
-        Que, por estos lado', los pibes la viven
-        Mis olas de flow te dejan 20 metros
-        Debajo del mar hablando con delfines
-        Hago que se retiren y vuelvan tantas veces
-        A la music que les tienen que dar un Guinness
-        Los tengo enganchaos, drug dealer
-        Esta peli no la ve' en el cine
-        Lo que hacen en un año, lo hago en un finde
-        Mi hermano trajo misiles
-        Estábamos abajo, ahora estamos en la cima
-        Ropa de diseñador hecha a mi medida
-        Estoy con mi hermano jugando en las grandes ligas
-        Me espera el dinero si aterrizo en Argentina
-        La money me tiene despierto, gano dinero y lo invierto
-        Negociador experto, si no es de dinero, no entiendo
-        Follow, okey (yeah), brilla mi chain (yeah)
-        Otro champagne (yeah), ¿dónde está mi Moët?
-        Voy a beber otro Moët
-        Voy a compra' un Mercedes-Benz
-        Voy a gastar otro de 100
-        Voy a fumar en Calvin Klein
-        Voy a pasarme el puto juego
-        No me molesta estar primero
-        Voy a comprar un bolso nuevo
-        Solo para guardar dinero
-        Me destapé otro Moët
-        Mi perra viste de Chanel
-        Estamo' fresco', estamo' bien
-        Muchos billete' de 100
-        Ahora que estamo' en subida
-        Tenemo' otra perspectiva
-        Voy con el Duko y todo' miran
-        Las gatas nos aman, los gatos nos tiran
-        Me destapé otro Moët
-        Fumo y grabo en la suite del hotel (uh)
-        Me quieren dar y no me pueden ver
-        Ya no es top five, ahora es top tres
-        Ahora que estamos en subida
-        Todo' nos quieren en su vida
-        Voy con C.R.O y nos miran
-        Fumando queso, cheesy</p>
+    <div id="div1">
+        <div id="div3" class="">
+            ¿Quienes somos?<span class="flecha"><i class="bi bi-caret-down-fill"></i></span>
+        </div>
+        <div id="div2" class="">
+            Somos una aerolinea, dedicada a brindar asesoramiento integral en el rubro.
+            Nacimos con el sueño de ofrecer a los viajeros toda nuestra experiencia y capacidad en el área, brindando un servicio de alta calidad y adaptándonos a las exigencias de hoy.
+            Contamos con una amplia variedad de ofertas y paquetes turísticos personalizados que el cliente podrá evaluar y cotizar a través de nuestro sitio web o realizar consultas y dejar opiniones por nuestras redes sociales, respondiendo de manera inmediata a los requerimientos de los clientes.
+            Nuestra empresa está totalmente orientada hacia las necesidades del cliente, teniendo en cuenta sus preferencias y cultura.
+            Priorizamos siempre su seguridad y que sea una estadía memorable, por eso le ofrecemos comodidad y conveniencia económica en todo momento.
+            Valores: La capacitación, dedicación y óptima predisposición son las razones principales por las que esta agencia de viajes se ha desarrollado como una empresa singular y diferente al resto. 
+            La innovación permanente, la creatividad y la orientación al cliente son valores fundamentales que plasmamos en nuestra forma de trabajar, persiguiendo el objetivo de marcar la diferencia en todo momento.
+            Misión: nuestro objetivo es obtener la satisfacción y superar las expectativas de los clientes para seguir creciendo. Estamos convencidos de que cada tarea debe estar dirigida a dar respuesta a la confianza que ellos depositan en nosotros.
+            Visión: queremos ser la agencia de viajes líder en Argentina distinguida por la atención personalizada al cliente y la garantía de un viaje memorable en familia, en pareja o con amigos.
+            Ponemos a su disposición la solución integral para hacer de sus viajes por Argentina y el resto del mundo una experiencia única. Por ello, contamos con la infraestructura y la tecnología adecuada para responder de manera ágil a la demanda de los viajeros
+            Si su anhelo es vivir momentos maravillosos, confié en nosotros y prepárese para disfrutar de viajes inolvidables.
+        </div>
+    </div>
+
+    <script>
+        $("#div3").click(function(e) {
+            const div2 = document.getElementById('div2');
+            const div3 = document.getElementById('div3');
+            if (div2.classList.contains("active")) {
+
+                div2.classList.remove("active")
+                div3.classList.remove("actionated")
+                $("#div2").slideUp(200)
+                document.querySelector('.flecha').innerHTML = '<i class="bi bi-caret-down-fill"></i>';
+            } else {
+                $("#div2").slideDown(250)
+                div2.classList.add("active")
+                div3.classList.add("actionated")
+                document.querySelector('.flecha').innerHTML = '<i class="bi bi-caret-up-fill"></i>';
+            }
+        })
+    </script>
+
 </section>
